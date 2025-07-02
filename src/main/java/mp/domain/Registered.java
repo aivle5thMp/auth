cb45532f82714dcc8729abc655b86228542974dd
upz <smilish67@naver.com>
2025-07-02 21:52:19 +0900

    chore: edit kafka message

--- src/main/java/mp/domain/Registered.java
+++ src/main/java/mp/domain/Registered.java
@@ -3,7 +3,6 @@ package mp.domain;
 import java.util.UUID;
 import lombok.*;
 import mp.infra.AbstractEvent;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 //<<< DDD / Domain Event
 @Data
@@ -11,17 +10,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Registered extends AbstractEvent {
 
     private UUID id;
-    private String name;
-    private String email;
-    @JsonIgnore  // 비밀번호는 Kafka 메시지에서 제외
-    private String password;
-    private String role;
-    private Boolean isSubscribed;
 
     public Registered(User aggregate) {
-        super(aggregate);
-        // 보안상 비밀번호는 명시적으로 null로 설정
-        this.password = null;
+        super();
+        this.id = aggregate.getId();
     }
 
     public Registered() {
